da59b69241dc9d21055b3587ac05efd1e84c2c90
Wolfgang Kühn <wolfgang-kuehn@decatur.de>
2020-08-24 16:29:09 +0200

    bare import gen

--- dev_server/make.py
+++ dev_server/make.py
@@ -7,15 +7,13 @@ def process(source: Path, target: Path):
     if source.suffix in {'.html', '.js'}:
         print('>>>>>>>>' + source.as_posix())
         t = source.read_text(encoding='utf8')
-        # http://localhost:8000/gridchen/testing/suite.js
-        # http://localhost:8080/gridchen/testing/suite.js
-        t = re.sub(r'(import .*) ("|\')/gridchen/', r'\1 \2https://decatur.github.io/grid-chen/gridchen/', t)
+        t = re.sub(r'(import[^"\']*) ("|\')/gridchen/', r'\1 \2https://decatur.github.io/grid-chen/gridchen/', t, flags=re.DOTALL)
         target.write_text(t, encoding='utf8')
     else:
         shutil.copy2(p.as_posix(), target_root / elem)
 
 
-target_root = Path('./site')
+target_root = Path('./docs')
 
 for elem in Path('./').glob('*.*'):
     p = Path('./') / elem
