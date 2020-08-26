c6ccb365f8944fc22ab66055f2488b7b1dca0bea
Wolfgang Kühn <wolfgang-kuehn@decatur.de>
2020-08-26 12:37:03 +0200

    improved import mapper

--- dev_server/make.py
+++ dev_server/make.py
@@ -1,7 +1,7 @@
 import re
 import shutil
 from pathlib import Path
-from typing import List, Tuple, Generator
+from typing import List, Tuple, Generator, Dict
 
 
 def process_file(source: Path, target: Path, spec_mapping: List[Tuple[str, str]]):
@@ -15,11 +15,50 @@ def process_file(source: Path, target: Path, spec_mapping: List[Tuple[str, str]]
 
 
 def read_file(source: Path, spec_mapping: List[Tuple[str, str]]) -> str:
-    print('>>>>>>>>' + source.as_posix())
-    t = source.read_text(encoding='utf8')
-    for spec_map in spec_mapping:
-        t = re.sub(r'(import[^"\']*) (["\'])' + spec_map[0], r'\1 \2' + spec_map[1], t, flags=re.DOTALL)
-    return t
+    print('read_file ' + source.as_posix())
+    src = source.read_text(encoding='utf8')
+    was_patched, src = process_imports(src, spec_mapping)
+    # TODO: Cache processed modules
+    return src
+
+
+def process_imports(src: str, imports: Dict[str, str], scopes: dict = dict()) -> Tuple[str, bool]:
+    """
+    Remaps the imports and exports in the specified Javascript module according the import map.
+
+    :param src:
+    :param imports: See https://github.com/WICG/import-maps
+    :return: 
+    """
+
+    # Skip initial comments. Note this always matches.
+    m = re.match(r'(\s*(/\*.*?\*/|//[^\r\n]*))*', src, flags=re.DOTALL)
+    # print(m.group())
+    # index = 0
+    # for m in re.finditer(r'(\s*(/\*.*?\*/|//[^\r\n]*))*', src, flags=re.DOTALL):
+    #     print(m)
+    #     index = m.end()
+    #     print(m.group())
+    end_of_comments = m.end()
+    m = re.search(r'(function|const|let|var)\s', src[end_of_comments:], flags=re.DOTALL)
+    if m is None:
+        # TODO: Handle empty module
+        return (src, False)
+    print(m)
+    import_section = src[end_of_comments:end_of_comments + m.start()]
+    is_import_section = re.match('\s*(import|export)\s', import_section) is not None
+    if is_import_section:
+        was_patched = False
+        for key, value in imports.items():
+            import_section, patch_count = re.subn(r'((import|export).*?) (["\'])' + key, r'\1 \3' + value, import_section, flags=re.DOTALL)
+            was_patched |= patch_count > 0
+
+        if was_patched:
+            return (src[0:end_of_comments] + import_section + src[end_of_comments + m.start():], was_patched)
+        else:
+            return (src, False)
+    else:
+        return (src, False)
 
 
 def process_dir(source: Generator[Path, None, None], target_root: Path, spec_mapping: List[Tuple[str, str]]):
