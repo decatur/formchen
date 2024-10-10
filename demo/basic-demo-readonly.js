57450903ac5f04c0b7916b466d694ae7987d9086
Wolfgang <wo.kuehn@enbw.com>
2024-10-10 09:17:52 +0200

    add readonly demo

---
+++ demo/basic-demo-readonly.js
@@ -0,0 +1,91 @@
+/** @import { JSONSchema } from "../formchen/types.js" */
+
+import { createFormChen } from "../formchen/formchen.js"
+import { bindTabs } from "../test/utils.js";
+
+/** @type{JSONSchema} */
+const schema = {
+    "$defs": {
+        "measurements": {
+            type: 'array',
+            format: 'grid',
+            items: {
+                type: 'array',  // tuple schema
+                items: [
+                    { title: 'TimeStamp', width: 200, type: 'string', format: 'datetime', period: 'HOURS' },
+                    { title: 'Age [d]', width: 100, type: 'number' },
+                    { title: 'Weight [g]', width: 100, type: 'number' }
+                ]
+            }
+        }
+    },
+    title: 'BasicDemoReadOnly',
+    type: 'object',
+    readOnly: true,
+    properties: {
+        plant: {
+            title: 'Plant',
+            tooltip: 'The name of the plant',
+            type: 'string'
+        },
+        observer: {
+            title: 'Observer',
+            type: 'string',
+            enum: ['Frida Krum', 'Tilda Swift']
+        },
+        start: {
+            title: 'Started',
+            type: 'string',
+            format: 'datetime'
+        },
+        latitude: {
+            title: 'Latitude',
+            type: 'number',
+            unit: '[DD]',
+            fractionDigits: 5
+        },
+        longitude: {
+            title: 'Longitude',
+            type: 'number',
+            unit: '[DD]',
+            fractionDigits: 5
+        },
+        measurements: {
+            title: 'Daylight Measurements',
+            $ref: '#/$defs/measurements'
+        },
+        isCompleted: {
+            title: 'Is Completed',
+            type: 'boolean'
+        }
+    }
+};
+
+const data = {
+    plant: 'Rubus idaeus',
+    observer: 'Frida Krum',
+    start: '2019-01-01T00:00Z',
+    latitude: 41.40338,
+    longitude: 2.17403,
+    measurements: [
+        ["2019-01-01T00:00Z", 0, 0],
+        ["2019-02-01T00:00Z", 1, 2],
+        ["2019-03-01T00:00Z", 2, 4]
+    ],
+    isCompleted: true
+};
+
+const formElement = document.getElementById(schema.title);
+bindTabs(formElement, schema, value, patch);
+const formchen = createFormChen(formElement, schema, data);
+
+function value() {
+    return formchen.value
+}
+
+function patch() {
+    return formchen.patch
+}
+
+
+
